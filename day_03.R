096a30d804075d0e12e507a30d03cf83eecb7213
FunTanya <95546462+FunTanya@users.noreply.github.com>
2021-12-04 21:18:34 +0100

    Add files via upload

---
+++ day_03.R
@@ -0,0 +1,21 @@
+zeros.ones <- readLines("data/input_03.txt")
+n <- max(nchar(zeros.ones))
+oxygen <- Co2 <- read.fwf(textConnection(zeros.ones), rep(1, n))
+for (i in 1:(n)){
+ones <- sum(oxygen[,i])
+zeros <- sum(!oxygen[,i])
+if (ones<zeros){
+oxygen <- oxygen[oxygen[,i]==0,]
+} else oxygen <- oxygen[oxygen[,i]==1,]
+if (nrow(oxygen)==1) break
+}
+for (i in 1:n){
+ones <- sum(Co2[,i])
+zeros <- sum(!Co2[,i])
+if (ones<zeros){
+Co2 <- Co2[Co2[,i]==1,]
+} else Co2 <- Co2[Co2[,i]==0,]
+if (nrow(Co2)==1) break
+}
+strtoi(paste(oxygen, collapse = ""), base=2)*
+strtoi(paste(Co2, collapse = ""), base=2)
