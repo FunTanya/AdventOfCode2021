f8b4bf3745ac5591199419adb321b5b00145badb
FunTanya <95546462+FunTanya@users.noreply.github.com>
2021-12-05 08:30:48 +0100

    Update day_05.R

--- day_05.R
+++ day_05.R
@@ -3,7 +3,7 @@ paths <- read.table(text = gsub("->", ",", readLines("data/input_05.txt")),sep="
 colnames(paths) <- c("x1","y1","x2","y2")
 data <- inc(paths)
 diagram <- matrix(0,max(data),max(data))
-
+puzzle.one <- FALSE #TRUE for puzzle one
 attach(data)
 for (i in (1:nrow(paths))){
   if (y1[i]==y2[i])
@@ -11,11 +11,12 @@ for (i in (1:nrow(paths))){
   else
     if (x1[i]==x2[i])
       inc(diagram[y1[i]:y2[i],x1[i]])
-    else {
-      x.move <- x2[i] - x1[i]
-      y.move <- y2[i] - y1[i]
-      inc(diag(diagram[y1[i] + (0:y.move), x1[i] + (0:x.move)]))
-    }
+    else if (puzzle.one == TRUE) {}
+      else {
+        x.move <- x2[i] - x1[i]
+        y.move <- y2[i] - y1[i]
+        inc(diag(diagram[y1[i] + (0:y.move), x1[i] + (0:x.move)]))
+      }
 }
 detach(data)
 sum(diagram>1)
