652b202b5a7d0599a89b79afaf4fa97aed70549b
FunTanya <95546462+FunTanya@users.noreply.github.com>
2021-12-17 10:14:51 +0100

    Create day_17a.R

---
+++ day_17a.R
@@ -0,0 +1,30 @@
+target.x.min <- 169#20
+target.x.max <- 206#30
+target.y.min <- -108#-10
+target.y.max <- -68#-5
+max.y <- max <- -Inf
+n <- 200 #hruba sila
+for( move.x in (1:n)){
+  for( move.y in (-n:n)){
+  move.x.ktere <- move.x
+  move.y.ktere <- move.y
+  max.y <-(-Inf) 
+  x <- y <- 0 
+  repeat{
+    x <- x+move.x.ktere
+    y <- y+move.y.ktere
+    move.x.ktere <- move.x.ktere-1*sign(move.x.ktere)
+    move.y.ktere <- move.y.ktere-1
+    if (y>max.y ) max.y <- y
+    if ((x>(target.x.min-1))&(x<(target.x.max+1))){
+        if ((y>(target.y.min-1)) & (y<(target.y.max+1))){
+          if (max.y>max){max <- max.y}
+          break
+          }
+      }
+     if (x>target.x.max) {break}
+     if (y<(target.y.min)) {break}
+ }
+  }
+}
+print(max)
