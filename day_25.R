ae2b382a25b9e5976a34330c4365c94aa46c6886
FunTanya <95546462+FunTanya@users.noreply.github.com>
2021-12-26 18:40:55 +0100

    Add files via upload

---
+++ day_25.R
@@ -0,0 +1,36 @@
+MyFile <- "data/input_25.txt"
+#MyFile <- "testdata/test_25.txt"
+data <- readLines(MyFile)
+n <- max(nchar(data))
+data <- matrix(unlist(strsplit(data,split = "")),ncol = n,byrow = TRUE)
+count <- 0
+repeat{
+  change.1 <- FALSE
+  change.2 <- FALSE
+  count <- count+1
+  novadata <- data
+  for (i in 1:nrow(data)){
+    for (j in 1:ncol(data)){
+      jindex <- j+1
+      if (j==ncol(data)){jindex <- 1}
+      if ((data[i,j]==">") & (data[i,jindex]==".")){
+        novadata[i,j] <- "."
+        novadata[i,jindex] <- ">"
+        change.1 <- TRUE
+        }
+    }
+  }
+  data <- novadata
+  for (i in 1:nrow(data)){
+    for (j in 1:ncol(data)){
+      iindex <- i+1
+      if (i==nrow(novadata)){iindex <- 1}
+      if ((novadata[i,j]=="v") & (novadata[iindex,j]==".")){
+        data[i,j] <- "."
+        data[iindex,j] <- "v"
+        change.2 <- TRUE
+      }
+    }
+  }
+  if (!change.1 & !change.2){print(count);break}
+}
