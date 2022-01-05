864de1cac0786f357ef67257ed66d4d6ef2fa8ae
FunTanya <95546462+FunTanya@users.noreply.github.com>
2022-01-05 13:54:23 +0100

    Update day_20.R

--- day_20.R
+++ day_20.R
@@ -1,33 +1,28 @@
 MyFile <- "data/input_20.txt"
-#  MyFile <- "testdata/test_20.txt"
-BinToDec <- function(x){
-  sum(2^(which(rev(unlist(strsplit(as.character(x), "")) == 1))-1))}
-repl <- function(mat33){
-  temp <- paste0(t(mat33),collapse = "")
-  kuk <- gsub("#","1",temp)
-  kuk <- gsub("\\.","0",kuk)
-  return(alg[BinToDec(kuk)+1])
-}
 alg <- unlist(strsplit(readLines(MyFile,n = 1),split = ""))
+alg <- gsub("#","1",alg)
+alg <- gsub("\\.","0",alg)
 x <- readLines(MyFile)
 x <- x[3:length(x)]
+x <- gsub("#","1",x)
+x <- gsub("\\.","0",x)
 prvni <- matrix(unlist(strsplit(x,split = "")),nr=length(x),byrow = TRUE)
- 
-for (i in 1:2) {prvni <- cbind(".",rbind(".",prvni,"."),".")}
-for (k in 1:50){
-    nx <- nrow(prvni)
-    dalsi <- prvni 
-    for (i in (2:(nx-1))){
-      for (j in (2:(nx-1))){
-        x33 <- prvni[(i-1):(i+1),(j-1):(j+1)]
-        dalsi[i,j]<- repl(x33)
-      }
-    }   
-    if (k%%2==1){ 
-      prvni <- cbind("#",rbind("#",dalsi[2:(nx-1),2:(nx-1)],"#"),"#")
-      prvni <- cbind("#",rbind("#",prvni,"#"),"#")
-      } else{
-      prvni <- cbind(".",rbind(".",dalsi[2:(nx-1),2:(nx-1)],"."),".")
-      prvni <- cbind(".",rbind(".",prvni,"."),".")}
+for (i in 1:2) {prvni <- cbind(0,rbind(0,prvni,0),0)}
+
+for (k in 1:50){ #50 or 2
+  nx <- nrow(prvni)
+  dalsi <- prvni 
+  for (i in (2:(nx-1))){
+    for (j in (2:(nx-1))){
+      x33 <- prvni[(i-1):(i+1),(j-1):(j+1)]
+      dalsi[i,j]<- alg[strtoi(paste0(t(x33),collapse = ""), base = 2)+1]
+    }
+  }   
+  if (k%%2==1){ 
+    prvni <- cbind(1,rbind(1,dalsi[2:(nx-1),2:(nx-1)],1),1)
+    prvni <- cbind(1,rbind(1,prvni,1),1)
+  } else{
+    prvni <- cbind(0,rbind(0,dalsi[2:(nx-1),2:(nx-1)],0),0)
+    prvni <- cbind(0,rbind(0,prvni,0),0)}
 }
-table(prvni)
+table(prvni)[2]
