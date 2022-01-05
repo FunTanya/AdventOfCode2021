c3cb5554bb987f39f8b539969b3190c0d07e0982
FunTanya <95546462+FunTanya@users.noreply.github.com>
2022-01-05 11:50:58 +0100

    Update day_17a.R

--- day_17a.R
+++ day_17a.R
@@ -1,30 +1,32 @@
-target.x.min <- 169#20
-target.x.max <- 206#30
-target.y.min <- -108#-10
-target.y.max <- -68#-5
+sentence <- readLines("data/input_17.txt")
+library(strex)
+target.x.min <- str_nth_number(sentence, n = 1,negs = TRUE)
+target.x.max <- str_nth_number(sentence, n = 2,negs = TRUE)
+target.y.min <- str_nth_number(sentence, n = 3,negs = TRUE)
+target.y.max <- str_nth_number(sentence, n = 4,negs = TRUE)
 max.y <- max <- -Inf
 n <- 200 #hruba sila
 for( move.x in (1:n)){
   for( move.y in (-n:n)){
-  move.x.ktere <- move.x
-  move.y.ktere <- move.y
-  max.y <-(-Inf) 
-  x <- y <- 0 
-  repeat{
-    x <- x+move.x.ktere
-    y <- y+move.y.ktere
-    move.x.ktere <- move.x.ktere-1*sign(move.x.ktere)
-    move.y.ktere <- move.y.ktere-1
-    if (y>max.y ) max.y <- y
-    if ((x>(target.x.min-1))&(x<(target.x.max+1))){
+    move.x.ktere <- move.x
+    move.y.ktere <- move.y
+    max.y <-(-Inf) 
+    x <- y <- 0 
+    repeat{
+      x <- x+move.x.ktere
+      y <- y+move.y.ktere
+      move.x.ktere <- move.x.ktere-1*sign(move.x.ktere)
+      move.y.ktere <- move.y.ktere-1
+      if (y>max.y ) max.y <- y
+      if ((x>(target.x.min-1))&(x<(target.x.max+1))){
         if ((y>(target.y.min-1)) & (y<(target.y.max+1))){
           if (max.y>max){max <- max.y}
           break
-          }
+        }
       }
-     if (x>target.x.max) {break}
-     if (y<(target.y.min)) {break}
- }
+      if (x>target.x.max) {break}
+      if (y<(target.y.min)) {break}
+    }
   }
 }
 print(max)
