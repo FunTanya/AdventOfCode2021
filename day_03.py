ad52f9d0f160e0bdb7f000c8df8ca7bef8fb8aa0
FunTanya <tatana@funiokova.cz>
2022-01-12 21:06:52 +0100

    My Day03.py

--- day_03.py
+++ day_03.py
@@ -1,30 +1,69 @@
 # part 1
+def GammaToDec(x):
+    string_ints = [str(ints) for ints in x]
+    str_of_ints = "".join(string_ints)
+    return int(str_of_ints,2)
+
 data = []
-f = open("data/input_03.txt")
+f = open("data/input_03.txt",'r')
 for line in f:
     x = [int(i) for i in line.rstrip()]
     data.append(x)
+n=len(data)
 gamma =[]
-for j in range(0,12):
+for j in range(len(data[0])):
     sum0 = 0
-    for i in range(len(data)):
+    for i in range(n):
         if data[i][j] == 0:
             sum0 +=1
-    if sum0>len(data)/2:
-        numero = 0
+    if sum0>  n/2:
+        gamma.append(0)
     else:
-        numero = 1
-    gamma.append(numero)
-print(gamma)
-reverse = [1 - number  for number in gamma]
-print(reverse)
+        gamma.append(1)
+gamma_reverse = [1 - i  for i in gamma]
 
-string_ints = [str(int) for int in gamma]
-str_of_ints = "".join(string_ints)
-first = int(str_of_ints,2)
+print(GammaToDec(gamma)*GammaToDec(gamma_reverse))
+# part 2
+oxygen = C02 = []
+j=0 # koukam na nultou cifru....
+temp_data = data
+while True:
+    sum0 = 0
+    for i in range(len(temp_data)): #zbyvajici data
+        if temp_data[i][j] == 0:
+            sum0 +=1
+    if sum0>len(temp_data)/2:
+        most_common = 0
+    else:
+        most_common = 1
+    oxygen=[]
+    for i in range(len(temp_data)):
+        if temp_data[i][j] == most_common:
+            oxygen.append(temp_data[i]) 
+    if len(oxygen)==1:
+        break
+    else: 
+        temp_data=oxygen
+        j+=1
 
-string_ints = [str(int) for int in reverse]
-str_of_ints = "".join(string_ints)
-second = int(str_of_ints,2)
-
-print(first*second) 
+j=0 # koukam na nultou cifru....
+temp_data = data
+while True:
+    sum0 = 0
+    for i in range(len(temp_data)): #zbyvajici data
+        if temp_data[i][j] == 0:
+            sum0 +=1
+    if sum0>len(temp_data)/2:
+        less_common = 1
+    else:
+        less_common = 0
+    CO2=[]
+    for i in range(len(temp_data)):
+        if temp_data[i][j] == less_common:
+            CO2.append(temp_data[i]) 
+    if len(CO2)==1:
+        break
+    else: 
+        temp_data=CO2
+        j+=1
+print(GammaToDec(oxygen[0])*GammaToDec(CO2[0]))
