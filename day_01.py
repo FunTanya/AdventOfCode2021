23ead36102642e30a1ce863f1016f65209f9938b
tana funiokova <dobeskyd@seznam.cz>
2022-01-12 12:02:45 +0100

    My First Python

---
+++ day_01.py
@@ -0,0 +1,17 @@
+f = open("input_01.txt")
+data = f.readlines()
+depths = [int(i) for i in data]
+def Mycounter(x):
+    count = 0
+    for i in range(0, len(x)-1):
+        diff = x[i+1]-x[i]
+        if diff>0:
+            count +=1
+    return count
+print(Mycounter(depths))
+
+# part 2
+sum3=[]
+for i in range(0, len(depths)-2):
+    sum3.append(depths[i]+depths[i+1]+depths[i+2])
+print(Mycounter(sum3))   
