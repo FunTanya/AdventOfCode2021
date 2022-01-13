bd520235e9f45786fd8b28b7b1c33ac7af5fd86c
tana funiokova <dobeskyd@seznam.cz>
2022-01-13 15:10:38 +0100

    Day 3 v1.0

--- day_03.py
+++ day_03.py
@@ -1,14 +1,18 @@
-# part 1
-def GammaToDec(x):
-    string_ints = [str(ints) for ints in x]
-    str_of_ints = "".join(string_ints)
-    return int(str_of_ints,2)
+from itertools import chain
+
+def BitsToDec(x):
+    list_of_str_digits = [str(digit) for digit in x]
+    str_digits = "".join(list_of_str_digits)
+    return int(str_digits,2)
 
 data = []
 f = open("data/input_03.txt",'r')
 for line in f:
-    x = [int(i) for i in line.rstrip()]
+    x = [int(i) for i in line.strip()]
     data.append(x)
+
+
+# part 1
 n=len(data)
 gamma =[]
 for j in range(len(data[0])):
@@ -16,54 +20,37 @@ for j in range(len(data[0])):
     for i in range(n):
         if data[i][j] == 0:
             sum0 +=1
-    if sum0>  n/2:
+    if sum0 > n/2:
         gamma.append(0)
     else:
         gamma.append(1)
-gamma_reverse = [1 - i  for i in gamma]
+        
+gamma_reverse = [1 - bit  for bit in gamma]
+print(BitsToDec(gamma)*BitsToDec(gamma_reverse))
 
-print(GammaToDec(gamma)*GammaToDec(gamma_reverse))
 # part 2
-oxygen = C02 = []
-j=0 # koukam na nultou cifru....
-temp_data = data
-while True:
-    sum0 = 0
-    for i in range(len(temp_data)): #zbyvajici data
-        if temp_data[i][j] == 0:
-            sum0 +=1
-    if sum0>len(temp_data)/2:
-        most_common = 0
-    else:
-        most_common = 1
-    oxygen=[]
-    for i in range(len(temp_data)):
-        if temp_data[i][j] == most_common:
-            oxygen.append(temp_data[i]) 
-    if len(oxygen)==1:
-        break
-    else: 
-        temp_data=oxygen
-        j+=1
+def BitRating(data,target_val):
+    j=0 # start with first digit
+    temp_data = data
+    while True:
+        sum0 = 0
+        for i in range(len(temp_data)): #data after restriction
+            if temp_data[i][j] == 0:
+                sum0 +=1
+        if sum0>len(temp_data)/2:
+            less_common = target_val
+        else:
+            less_common = 1-target_val
+        rating=[]
+        for i in range(len(temp_data)):
+            if temp_data[i][j] == less_common:
+                rating.append(temp_data[i]) 
+        if len(rating)==1:
+            break
+        else: 
+            temp_data=rating
+            j+=1
+    return BitsToDec(list(chain(*rating)))
+          
 
-j=0 # koukam na nultou cifru....
-temp_data = data
-while True:
-    sum0 = 0
-    for i in range(len(temp_data)): #zbyvajici data
-        if temp_data[i][j] == 0:
-            sum0 +=1
-    if sum0>len(temp_data)/2:
-        less_common = 1
-    else:
-        less_common = 0
-    CO2=[]
-    for i in range(len(temp_data)):
-        if temp_data[i][j] == less_common:
-            CO2.append(temp_data[i]) 
-    if len(CO2)==1:
-        break
-    else: 
-        temp_data=CO2
-        j+=1
-print(GammaToDec(oxygen[0])*GammaToDec(CO2[0]))
+print(BitRating(data, target_val = 1) * BitRating(data,target_val = 0))
